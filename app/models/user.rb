dc39e7f9bf29001922ab149f8cbeb25bab51c307
iamanishagarwal <iamanishagarwal@gmail.com>
2020-03-25 15:19:47 +0530

    feat: Setup bcrypt gem in users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_180923) do
+ActiveRecord::Schema.define(version: 2020_03_25_092503) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_180923) do
     t.string "experience"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "password_digest"
   end
 
 end
