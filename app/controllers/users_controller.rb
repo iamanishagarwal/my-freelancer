e2882a919f9680d653491abe0c7284339d43fed1
iamanishagarwal <iamanishagarwal@gmail.com>
2020-03-26 13:26:17 +0530

    fix: Fix issue with users destroy method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[edit update show]
+  before_action :set_user, only: %i[edit update show destroy]
   before_action :require_same_user, only: %i[edit update destroy]
 
   def index
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(create_user_params)
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = 'User Successfully signed up'
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   def edit; end
 
   def update
-    if @user.update(user_params)
+    if @user.update(update_user_params)
       flash[:success] = 'Your account was updated successfully'
       redirect_to user_path(@user)
     else
@@ -41,10 +41,10 @@ class UsersController < ApplicationController
   def show; end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
     flash[:danger] = 'User successfully deleted'
-    redirect_to users_path
+    redirect_to root_path
   end
 
   private
@@ -53,8 +53,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def user_params
+  def create_user_params
     params.require(:user).permit(:name, :email, :password, :role)
+  end
+
+  def update_user_params
+    params.require(:user).permit(:name, :email)
   end
 
   def require_same_user
